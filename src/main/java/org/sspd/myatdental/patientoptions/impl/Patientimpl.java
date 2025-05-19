8b17a6c7e33d1be2cdf726a514452c4348ae5623
SSPD Admin <hlainghtun2018@gmail.com>
2025-05-20 01:01:31 +0630

    UI Change

--- src/main/java/org/sspd/myatdental/patientoptions/impl/Patientimpl.java
+++ src/main/java/org/sspd/myatdental/patientoptions/impl/Patientimpl.java
@@ -1,4 +1,96 @@
 package org.sspd.myatdental.patientoptions.impl;
 
-public class Patientimpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+import org.sspd.myatdental.dto.DataAccessObject;
+import org.sspd.myatdental.patientoptions.model.Patient;
+
+import java.util.List;
+
+@Repository
+public class Patientimpl implements DataAccessObject<Patient> {
+
+    private final SessionFactory sessionFactory;
+
+    public Patientimpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Patient> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Patient ", Patient.class).list();
+        }
+    }
+
+    @Override
+    public boolean save(Patient patient) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx= session.beginTransaction();
+            session.persist(patient);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            assert tx != null;
+            tx.rollback();
+
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(Patient patient) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.remove(patient);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Patient patient) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.merge(patient);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Patient findById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Patient patient= session.get(Patient.class, id);
+            if (patient != null) {
+                session.remove(patient);
+                tx.commit();
+                return true;
+            }
+            tx.rollback();
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateById(Patient patient) {
+        return false;
+    }
 }
--- src/main/java/org/sspd/myatdental/patientoptions/service/PatientService.java
+++ src/main/java/org/sspd/myatdental/patientoptions/service/PatientService.java
@@ -1,4 +1,40 @@
 package org.sspd.myatdental.patientoptions.service;
 
+import org.springframework.stereotype.Service;
+import org.sspd.myatdental.patientoptions.impl.Patientimpl;
+import org.sspd.myatdental.patientoptions.model.Patient;
+
+import java.util.List;
+
+@Service
 public class PatientService {
+
+   private final  Patientimpl patientimpl;
+
+    public PatientService(Patientimpl patientimpl) {
+        this.patientimpl = patientimpl;
+    }
+
+    public List<Patient> getPatients() {
+
+        return patientimpl.findAll();
+
+    }
+
+    public boolean addTreatment(Patient patient) {
+        return patientimpl.save(patient);
+    }
+
+    public boolean updatePatient(Patient patient) {
+        return patientimpl.update(patient);
+    }
+
+    public boolean deletePatient(Patient patient) {
+        return patientimpl.delete(patient);
+    }
+
+    public boolean deleteById(int id) {
+        return patientimpl.deleteById(id);
+    }
+
 }
